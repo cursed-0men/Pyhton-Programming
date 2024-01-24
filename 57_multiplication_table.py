a6039d3282171ca5c505299810a3cca746e25001
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-01-24 23:40:59 +0530

    Update 57_multiplication_table.py

--- 57_multiplication_table.py
+++ 57_multiplication_table.py
@@ -4,3 +4,6 @@ n = int(input("n = "))
 
 for i in range(1,11):
       print(f"{n} * {i} = {n*i}")
+
+# we use f to dynamically replace value in each iteration .
+# we can also compare curly brackets {} with "%d","%f","%c"... of C language.
