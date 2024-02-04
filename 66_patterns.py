6e6a9b8b21ff186edd07a5c4e79247682b761273
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-02-04 18:01:40 +0530

    Create 66_patterns.py

---
+++ 66_patterns.py
@@ -0,0 +1,38 @@
+# PATTERNS
+
+
+# for i in range(0,5):
+#       print('* ' * 5)
+
+# it will print:
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+
+
+# for i in range(0,5):
+#       for j in range(0,5):
+#             if i >= j:
+#                   print('*',end=" ")
+#       print(" ")
+
+# it will print:
+# *  
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+
+for i in range(0,5):
+      print("* " * (i+1))
+
+# it will print : 
+# * 
+# * *
+# * * *
+# * * * *
+# * * * * *
